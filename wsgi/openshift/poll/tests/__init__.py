c5a68ca00f861cfd28860c1c3fc9a1d0f7df5d21
mebusw <mebusw@163.com>
2012-09-23 00:37:39 +0800

    split tests into test files

--- wsgi/openshift/poll/tests/__init__.py
+++ wsgi/openshift/poll/tests/__init__.py
@@ -1,2 +1,3 @@
-from poll.tests.tests import *
+from poll.tests.test_forms import *
 from poll.tests.test_models import *
+from poll.tests.test_views import *
--- wsgi/openshift/poll/tests/test_views.py
+++ wsgi/openshift/poll/tests/test_views.py
@@ -13,7 +13,7 @@ from poll.forms import PollVoteForm
 import logging
 logger = logging.getLogger('myproject.custom')
 
-class MTVTest(TestCase):
+class PollsViewTest(TestCase):
     # python manage.py dumpdata auth.User --indent=2 > poll/fixtures/admin_user.json
     fixtures = ['admin_user.json']
     
@@ -58,36 +58,6 @@ class MTVTest(TestCase):
         self.assertIn(poll1.question, response.content)
         self.assertIn(poll2.question, response.content)
     
-    def test_form_renders_poll_choices_as_radio_inputs(self):
-        # set up a poll with a couple of choices
-        poll1 = Poll(question='6 times 7', pub_date=timezone.now())
-        poll1.save()
-        choice1 = Choice(poll=poll1, choice='42', votes=0)
-        choice1.save()
-        choice2 = Choice(poll=poll1, choice='The Ultimate Answer', votes=0)
-        choice2.save()
-
-        # set up another poll to make sure we only see the right choices
-        poll2 = Poll(question='time', pub_date=timezone.now())
-        poll2.save()
-        choice3 = Choice(poll=poll2, choice='PM', votes=0)
-        choice3.save()
-
-        # build a voting form for poll1
-        form = PollVoteForm(poll=poll1)
-
-        # check it has a single field called 'vote', which has right choices:
-        self.assertEquals(form.fields.keys(), ['vote'])
-
-        # choices are tuples in the format (choice_number, choice_text):
-        self.assertEquals(form.fields['vote'].choices, [
-            (choice1.id, choice1.choice),
-            (choice2.id, choice2.choice),
-        ])
-
-        # check it uses radio inputs to render
-        self.assertIn('input type="radio"', form.as_p())
-
     def test_page_shows_choices_using_form(self):
         # set up a poll with choices
         poll1 = Poll(question='time', pub_date=timezone.now())
@@ -108,14 +78,4 @@ class MTVTest(TestCase):
         # by checking for the choice text
         self.assertIn(choice1.choice, response.content.replace('&#39;', "'"))
         self.assertIn(choice2.choice, response.content.replace('&#39;', "'"))
-        
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
-
-    def test_basic_multiple(self):
-        self.assertEqual(3 * 2, 6)
-        
+                
